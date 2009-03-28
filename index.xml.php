f7491c80945369d492ee9bd49eb6768afde39918
Tomasz Stachewicz <tomasz.stachewicz@aenima.pl>
2009-03-28 17:42:40 +0100

    own custom version of includes/functions_display.php, extra data (like ID of post)

--- index.xml.php
+++ index.xml.php
@@ -19,7 +19,8 @@ $phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './';
 $phpbb_root_path = '../' . $phpbb_root_path;
 $phpEx = substr(strrchr(__FILE__, '.'), 1);
 include($phpbb_root_path . 'common.' . $phpEx);
-include($phpbb_root_path . 'includes/functions_display.' . $phpEx);
+//include($phpbb_root_path . 'includes/functions_display.' . $phpEx);
+include('includes/functions_display.' . $phpEx);
 
 // Start session management
 $user->session_begin();
@@ -28,7 +29,7 @@ $user->setup('viewforum');
 
 $temp = $phpbb_root_path;
 //$phpbb_root_path = "http://localhost/bp3/";
-$phpbb_root_path = "/";
+//$phpbb_root_path = "/";
 display_forums('', $config['load_moderators']);
 $phpbb_root_path = $temp;
 
@@ -40,74 +41,6 @@ $total_users	= $config['num_users'];
 $l_total_user_s = ($total_users == 0) ? 'TOTAL_USERS_ZERO' : 'TOTAL_USERS_OTHER';
 $l_total_post_s = ($total_posts == 0) ? 'TOTAL_POSTS_ZERO' : 'TOTAL_POSTS_OTHER';
 $l_total_topic_s = ($total_topics == 0) ? 'TOTAL_TOPICS_ZERO' : 'TOTAL_TOPICS_OTHER';
-
-// Grab group details for legend display
-if ($auth->acl_gets('a_group', 'a_groupadd', 'a_groupdel'))
-{
-	$sql = 'SELECT group_id, group_name, group_colour, group_type
-		FROM ' . GROUPS_TABLE . '
-		WHERE group_legend = 1
-		ORDER BY group_name ASC';
-}
-else
-{
-	$sql = 'SELECT g.group_id, g.group_name, g.group_colour, g.group_type
-		FROM ' . GROUPS_TABLE . ' g
-		LEFT JOIN ' . USER_GROUP_TABLE . ' ug
-			ON (
-				g.group_id = ug.group_id
-				AND ug.user_id = ' . $user->data['user_id'] . '
-				AND ug.user_pending = 0
-			)
-		WHERE g.group_legend = 1
-			AND (g.group_type <> ' . GROUP_HIDDEN . ' OR ug.user_id = ' . $user->data['user_id'] . ')
-		ORDER BY g.group_name ASC';
-}
-$result = $db->sql_query($sql);
-
-$legend = array();
-while ($row = $db->sql_fetchrow($result))
-{
-	$colour_text = ($row['group_colour']) ? ' style="color:#' . $row['group_colour'] . '"' : '';
-	$group_name = ($row['group_type'] == GROUP_SPECIAL) ? $user->lang['G_' . $row['group_name']] : $row['group_name'];
-
-	if ($row['group_name'] == 'BOTS' || ($user->data['user_id'] != ANONYMOUS && !$auth->acl_get('u_viewprofile')))
-	{
-		$legend[] = '<span' . $colour_text . '>' . $group_name . '</span>';
-	}
-	else
-	{
-		$legend[] = '<a' . $colour_text . ' href="' . append_sid("{$phpbb_root_path}memberlist.$phpEx", 'mode=group&amp;g=' . $row['group_id']) . '">' . $group_name . '</a>';
-	}
-}
-$db->sql_freeresult($result);
-
-$legend = implode(', ', $legend);
-
-// Generate birthday list if required ...
-/*
-$birthday_list = '';
-if ($config['load_birthdays'] && $config['allow_birthdays'])
-{
-	$now = getdate(time() + $user->timezone + $user->dst - date('Z'));
-	$sql = 'SELECT user_id, username, user_colour, user_birthday
-		FROM ' . USERS_TABLE . "
-		WHERE user_birthday LIKE '" . $db->sql_escape(sprintf('%2d-%2d-', $now['mday'], $now['mon'])) . "%'
-			AND user_type IN (" . USER_NORMAL . ', ' . USER_FOUNDER . ')';
-	$result = $db->sql_query($sql);
-
-	while ($row = $db->sql_fetchrow($result))
-	{
-		$birthday_list .= (($birthday_list != '') ? ', ' : '') . get_username_string('full', $row['user_id'], $row['username'], $row['user_colour']);
-
-		if ($age = (int) substr($row['user_birthday'], -4))
-		{
-			$birthday_list .= ' (' . ($now['year'] - $age) . ')';
-		}
-	}
-	$db->sql_freeresult($result);
-}
-*/
 
 $template->set_custom_template('templates', 'webservice_xml');
 
@@ -136,7 +69,7 @@ $template->assign_vars(array(
 $template->assign_var('XML_HEADER', "<?xml version='1.0' encoding='UTF-8' ?" . ">\n");
 
 // Output page
-page_header($user->lang['INDEX']);
+//page_header($user->lang['INDEX']);
 
 header('Pragma: no-cache');
 header("Content-Type: application/xml; name=\"index.xml\"");
